be60c2da14facd2590545e02f7e53f4e52e1d3e5
joostlek <joostlek@outlook.com>
2022-05-26 18:26:29 +0200

    Add ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
 	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
 	rules: {
 		"object-curly-spacing": "error",
-		curly: "error"
+		curly: "error",
+		quotes: ["warn", "double"],
+		indent: "off",
+		"space-before-function-paren": "off",
+		"no-tabs": "off",
+		"no-mixed-spaces-and-tabs": "off"
 	}
 }
